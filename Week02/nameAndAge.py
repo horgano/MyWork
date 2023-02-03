afa3004c8bc5a57f3644184109285f3c7fb5a782
Niall Horgan <G00426047@atu.ie>
2023-02-03 18:12:30 +0000

    Week 2 Tasks

--- Week02/nameAndAge.py
+++ Week02/nameAndAge.py
@@ -1,16 +1,17 @@
 #Read in a name and age and out put sentence
 #I like to do it in 3 formats
 
-#name = input('Please enter your name: ')
-#age = int(input('Please enter your age: '))
+name = input('Please enter your name: ')
+age = int(input('Please enter your age: '))
 
-#print (f'Hello {name}, your age is {age}')
-#print ('Hello ' + name + ', your age is ' + str(age))
-#print ('Hello {}, your age is {}'.format(name,age))
+print (f'Hello {name}, your age is {age}')
+print ('Hello ' + name + ', your age is ' + str(age))
+print ('Hello {}, your age is {}'.format(name,age))
 
 #1 format with tab
 
-#print ('Hello {},\t your age is {}'.format(name,age))
+print ('Hello {},\t your age is {}'.format(name,age))
 
 num = 21 - 4
 print (num)
+
