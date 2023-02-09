72e1b11e7321fd60760862c61251a6886562b53a
Niall Horgan <G00426047@atu.ie>
2023-02-09 20:40:09 +0000

    Week 03 Tasks

---
+++ Week03/lab3.23_floor.py
@@ -0,0 +1,11 @@
+# floor.py 
+# Author Niall Horgan
+
+# Floors a number
+
+import math
+
+number = float(input("Enter a decimal number: "))
+floored_number  = math.floor(number)
+
+print (f'{number} rounded down is {floored_number}')
---
+++ Week03/lab3.24_convert.py
@@ -0,0 +1,15 @@
+# convert.py
+# Author Niall Horgan   
+
+# Reads in a negative or positive number
+# in dollars and cents and gives the absolute
+# number in cents
+
+import math
+
+dollarsandcents = float(input("Enter your figure of dollars and cents: "))
+cents = (dollarsandcents * 100)
+centceiling = int(math.ceil(cents))     # https://www.w3schools.com/python/module_math.asp
+absolute = abs(centceiling)                 # https://kodify.net/python/math/absolute-value/
+
+print (f"The absolute value in cents for the amount you entered is: {absolute}")
