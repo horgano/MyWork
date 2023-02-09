8703587cff69c60cc50342476cfe0c8bd0a56ec8
Niall Horgan <G00426047@atu.ie>
2023-02-09 20:53:59 +0000

    Week 03 Tasks

---
+++ Week03/lab3.31_length.py
@@ -0,0 +1,9 @@
+# length.py
+# Author Niall Horgan
+
+# Read in a string and output length of the string
+
+input_string = input("Enter a string: ")
+length_of_string = len(input_string)
+
+print (f"The length of \"{input_string}\" string is: {length_of_string}")
