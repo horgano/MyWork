6c80594f8a837b50a8416e11a56b2940c33ac5d1
Niall Horgan <G00426047@atu.ie>
2023-02-09 20:45:57 +0000

    Week 03 Tasks

--- Week03/lab3.24_convert.py
+++ Week03/lab3.24_convert.py
@@ -10,6 +10,9 @@ import math
 dollarsandcents = float(input("Enter your figure of dollars and cents: "))
 cents = (dollarsandcents * 100)
 centceiling = int(math.ceil(cents))     # https://www.w3schools.com/python/module_math.asp
-absolute = abs(centceiling)                 # https://kodify.net/python/math/absolute-value/
+#absolute = abs(centceiling)            # https://kodify.net/python/math/absolute-value/
 
+absolute  = int(math.fabs(centceiling)) # https://kodify.net/python/math/absolute-value/
+
+#print (absolute)
 print (f"The absolute value in cents for the amount you entered is: {absolute}")
