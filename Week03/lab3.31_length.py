6435598282f39cec751ef8a5fe7251b88b5d601b
Niall Horgan <G00426047@atu.ie>
2023-02-09 20:58:20 +0000

    Week 03 Tasks

--- Week03/lab3.31_length.py
+++ Week03/lab3.31_length.py
@@ -3,7 +3,12 @@
 
 # Read in a string and output length of the string
 
-input_string = input("Enter a string: ")
-length_of_string = len(input_string)
+#input_string = input("Enter a string: ")
+#length_of_string = len(input_string)
 
-print (f"The length of \"{input_string}\" string is: {length_of_string}")
+#print (f"The length of \"{input_string}\" string is: {length_of_string}")
+
+
+# for fun
+message = 'John said\t"hi"\nI said\t\t"bye"'
+print (message)
