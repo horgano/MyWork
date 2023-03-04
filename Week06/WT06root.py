6be230883e0c3d1642ad9b8952102c673ae5121b
Niall Horgan <G00426047@atu.ie>
2023-03-04 17:47:05 +0000

    Week 06 Tasks

--- Week06/WT06root.py
+++ Week06/WT06root.py
@@ -10,4 +10,11 @@ square_root = math.sqrt(number)
 
 print (f"The square root of {number} is {square_root}")
 
-fvfvfv
+https://www.geeksforgeeks.org/find-root-of-a-number-using-newtons-method/
+https://tutorialsinhand.com/Articles/python-program-to-find-square-root-of-a-number-using-newton-square-root-formula.aspx#:~:text=If%20a%20given%20number%20is,correct%20square%20root%20of%20N.
+https://hackernoon.com/calculating-the-square-root-of-a-number-using-the-newton-raphson-method-a-how-to-guide-yr4e32zo
+https://medium.com/@sddkal/newton-square-root-method-in-python-270853e9185d
+https://www.geeksforgeeks.org/find-root-of-a-number-using-newtons-method/
+
+operator 
+https://docs.python.org/3/library/operator.html
