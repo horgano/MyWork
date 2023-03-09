c46b72e9bb2be34f408a378a54eb21ba0ef2875f
Niall Horgan <G00426047@atu.ie>
2023-03-09 18:37:38 +0000

    Week 07 Task

---
+++ Week07/file.py
@@ -0,0 +1,19 @@
+
+
+FILENAME = "data.txt"
+
+'''
+with open (FILENAME, "wt") as f:
+    for data in f:
+        # print (data, end"")
+        print (data.strip())
+        print (data[:-1])
+'''
+        
+with open ("data2.txt", "a") as f:
+    f.write ("what the hell\n")
+    f.write ("brown cow\n")
+
+data = 
+
+print ("done")
